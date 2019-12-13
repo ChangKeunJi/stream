d00d29a9e8d8a6f74a2af172fb78ed40961fa1ab
HellooCK <46767604+HellooCK@users.noreply.github.com>
2019-12-13 12:18:19 +0000

    create fake api server

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../apis/streams";
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
-    // console.log(input);
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  // rnderInput function gets called in Field Component.
+  // And It doesn't know what is "this" exactly in renderError.
+  // So we change renderInput function to Arrow function.
+  renderInput = ({ input, label, meta }) => {
+    // console.log(input, label);
+    // console.log(meta);
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit(formValue) {
     console.log(formValue);
@@ -18,9 +36,10 @@ class StreamCreate extends Component {
 
   render() {
     // console.log(this.props);
+    // Because of Redux Form, Lot of properties passed to this.props.
     return (
       <form
-        className="ui form"
+        className="ui form error"
         onSubmit={this.props.handleSubmit(this.onSubmit)}
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
@@ -35,23 +54,30 @@ class StreamCreate extends Component {
   }
 }
 
+// Valid input => Empty object
+// Invalid input => Key-Value pair + Error message.
 const validate = formValues => {
+  const errors = {};
+
   if (!formValues.title) {
-    const errors = {};
-
-    if (!formValues.title) {
-      errors.title = "You muse enter a title";
-    }
-
-    if (!formValues.description) {
-      errors.description = "You must enter a description";
-    }
+    errors.title = "You must enter a title";
   }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
 };
+
+// Validate gets called when Form is rendered OR
+// User interacts with it about anything.
+
+// Name property of Field is connected with errors object.
 
 export default reduxForm({
   form: "streamCreate",
-  validate: validate
+  validate
 })(StreamCreate);
 
 // How Redux Form works?
